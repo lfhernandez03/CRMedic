b2ea70e7e0ab32281d355cb414ef6a722335d5e9
lfhernandez03 <Luishsg1@gmail.com>
2025-06-18 10:05:16 -0500

    fix: vite config

---
+++ config/vite.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'dev_server' => [
+        'enabled' => false,
+    ],
+
+    'build_directory' => 'build',
+
+    'asset_url' => env('ASSET_URL', null),
+];
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\TestEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin/test-email', [TestEmailController::class, 'sendTestEmail']);
-Route::redirect('/', '/admin/login');
+
