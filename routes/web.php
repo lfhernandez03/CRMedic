3ceba30a0144524b24adb83d8b4e4ef76964980b
lfhernandez03 <Luishsg1@gmail.com>
2025-06-17 22:30:13 -0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TestEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin/test-email', [TestEmailController::class, 'sendTestEmail']);
+Route::redirect('/', '/admin/login');
